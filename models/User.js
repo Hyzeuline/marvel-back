703b8a80875ed1ef2423e5d96aae8053ad676210
hyzeuline <iseline.voison@gmail.com>
2025-09-07 11:23:03 +0200

    modèle user

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const User = mongoose.model("User", {
-  username: String,
   email: String,
+  account: { username: String },
   token: String,
   hash: String,
   salt: String,
